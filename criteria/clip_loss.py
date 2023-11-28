81aec97bbf9d9a9d0ffe88bf4576757b6733c138
Zinan Chi <31479492+Frederica97@users.noreply.github.com>
2023-11-28 02:36:42 -0500

    F.interpolate

--- criteria/clip_loss.py
+++ criteria/clip_loss.py
@@ -12,7 +12,8 @@ class CLIPLoss(torch.nn.Module):
         #self.avg_pool = torch.nn.AvgPool2d(kernel_size=opts.stylegan_size // 32)
 
     def forward(self, image, text):
-        image = torch.nn.functional.upsample_bilinear(image, (224, 224))
+        # image = torch.nn.functional.upsample_bilinear(image, (224, 224))
+        image = torch.nn.functional.interpolate(image, size=(224, 224), mode='bilinear', align_corners=False)
         #image = self.avg_pool(self.upsample(image))
         similarity = 1 - self.model(image, text)[0] / 100
         return similarity#.mean()
